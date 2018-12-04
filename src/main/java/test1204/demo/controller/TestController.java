01ea40f416d0f86b0f9d6f2d43951a3925107f94
武强 <wuqiang@wuqiangdeMacBook-Pro.local>
2018-12-04 16:19:48 +0800

    first commit

---
+++ src/main/java/test1204/demo/controller/TestController.java
@@ -0,0 +1,35 @@
+package test1204.demo.controller;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import test1204.demo.config.Producer;
+
+import java.io.UnsupportedEncodingException;
+
+@RestController
+public class TestController {
+    @Autowired
+    private Producer producer;
+
+    @RequestMapping("/push")
+    public String pushMsg(String msg){
+        try {
+            return producer.send("PushTopic","push",msg);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (RemotingException e) {
+            e.printStackTrace();
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        } catch (MQBrokerException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "ERROR";
+    }
+}
